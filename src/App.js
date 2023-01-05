b0033cf3edbcceb6d61ede27f3cc73d3bc8217bc
Kajsia Anderson <kajsia.anderson@gmail.com>
2023-01-05 16:02:17 -0700

    added nav buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import Header from './components/Header';
+import { Routes, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import AdminScreen from './screens/AdminScreen';
+import ProductScreen from './screens/ProductScreen';
+import AuthScreen from './screens/AuthScreen';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Routes>
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/auth" element={<AuthScreen />} />
+      <Route path="/product" element={<ProductScreen />} />
+      <Route path="/admin" element={<AdminScreen />} />
+     </Routes>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import {BiMenu} from 'react-icons/bi'
+import React, { useState } from 'react'
+import { BiMenu } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false)
+
+  const handleMenu = () => setShowMenu(!showMenu)
+
   return (
     <header>
-        <h2>Avanto</h2>
-        <BiMenu size="2rem" />
+      <h2>Avanto</h2>
+      <BiMenu size="2rem" style={{ zIndex: 3 }} onClick={handleMenu} />
+      <nav className={showMenu ? 'menu-show' : 'menu-hide'}>
+        <Link to="/">
+          <button onClick={handleMenu}>Home</button>
+        </Link>
+        <Link to="/auth">
+          <button onClick={handleMenu}>Login</button>
+        </Link>
+      </nav>
     </header>
   )
 }
